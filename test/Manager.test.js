379c5b3dccee6da401c095ffa44211fb897c4f52
Anthony Barragan <anthony.bar.89@gmail.com>
2022-02-21 14:00:50 -0800

    added Manager Object and passed tests

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -4,9 +4,17 @@ const Manager = require('../lib/Manager');
 
 
 test("creates a manager instance", () => {
-    const newMan = new Manager('dan', 90, 'dandaman@gmail.com')
+    const newMan = new Manager('dan', 90, 'dandaman@gmail.com', 439)
     expect(newMan.name).toBe('dan');
     expect(newMan.Id).toBe(90);
     expect(newMan.email).toBe('dandaman@gmail.com');
+    expect(newMan.officeNumber).toBe(439);
 });
+test("gets role from Manager", () => {
+    const newMan = new Manager();
+    expect(newMan.getRole()).toBe("Manager");
+})
+
+
+
 
