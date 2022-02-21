f0f35838d9f875bb5ee0521bec5162b6687988cd
Anthony Barragan <anthony.bar.89@gmail.com>
2022-02-21 15:33:48 -0800

    added intern object

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -3,6 +3,19 @@
 const Intern = require("../lib/Intern");
 
 test("create an Intern instance", () => {
-    const newInt = new Intern("James", 293, "james@gmail.com", "Loyola Mary Mount")
-    expect(newInt.name).
+    const newInt = new Intern("James", 293, "james@gmail.com", "Loyola Marymount")
+    expect(newInt.name).toBe("James");
+    expect(newInt.Id).toBe(293);
+    expect(newInt.email).toBe("james@gmail.com");
+    expect(newInt.school).toBe("Loyola Marymount");
+});
+
+test("Intern's Methods", () => {
+    const newInt = new Intern("James", 293, "james@gmail.com", "Loyola Marymount");
+    expect(newInt.getSchool()).toBe("Loyola Marymount");
+});
+
+test("get Intern role", () => {
+    const newInt = new Intern("James", 293, "james@gmail.com", "Loyola Mary Mount");
+    expect(newInt.getRole()).toBe("Intern");
 });
