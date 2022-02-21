1e47aae2acc60da06f8092040ab1346bc6262e96
Anthony Barragan <anthony.bar.89@gmail.com>
2022-02-21 15:19:17 -0800

    added engineer object and passed tests

---
+++ test/Engineer.test.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const Engineer = require('../lib/engineer');
+describe("make an engineer object", () => {
+    test("creates a engineer instance", () => {
+        const newEng = new Engineer('dan', 90, 'dandaman@gmail.com', 'abarragan89')
+        expect(newEng.name).toBe('dan');
+        expect(newEng.Id).toBe(90);
+        expect(newEng.email).toBe('dandaman@gmail.com');
+        expect(newEng.github).toBe('abarragan89');
+    });
+})
+
+describe("Engineer's Methods", () => {
+    test("gets role from engineer", () => {
+        const newEng = new Engineer();
+        expect(newEng.getRole()).toBe("Engineer");
+    })
+    test("get Github from Engineer", () => {
+        const newEng = new Engineer('dan', 90, 'dandaman@gmail.com', 'abarragan89');
+        expect(newEng.getGithub()).toBe("https://github.com/abarragan89")
+    });
+})
+
---
+++ test/Intern.test.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const Intern = require("../lib/Intern");
+
+test("create an Intern instance", () => {
+    const newInt = new Intern("James", 293, "james@gmail.com", "Loyola Mary Mount")
+    expect(newInt.name).
+});
